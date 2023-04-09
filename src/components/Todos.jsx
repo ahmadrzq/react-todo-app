92bf5f9cda7f04bec3c631452323f0e095e63efa
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-04-10 05:57:00 +0700

    Using context

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,20 +1,11 @@
 import React from "react";
 import TodoItem from "./TodoItem";
-// const Todos = () => {
-//     return (
-//         <div>
-//            {props.todos.map((todo)=>{
-//             return <p key={todo.id}>{todo.title}</p>
-//            })}
-//         </div>
-//     );
-// }
 
-const Todos = ({ todos, toggleCompleted, deleteTodo }) => {
+const Todos = ({ todos }) => {
     return (
         <div style={styles.container}>
             {todos.map((todo) => {
-                return <TodoItem key={todo.id} todo={todo} toggleCompleted={toggleCompleted} deleteTodo={deleteTodo}/>
+                return <TodoItem key={todo.id} todo={todo} />
             })}
         </div>
     )
